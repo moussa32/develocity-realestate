b147fc5a4cc7c1c4710a3d5e24675ca684548406
mozes32 <moussaebrahem32@gmail.com>
2022-09-20 17:09:18 +0200

    remove Credentials

--- src/api/constants.js
+++ src/api/constants.js
@@ -4,7 +4,6 @@ const BASE_URL = "https://akarat.develocity.app/api/";
 const APP_API_KEY = "wqrzIJIu5MrealstatedFYewn!%^&*Xu0@~dkqwlfYWSMqW6VQJQJjsOfMoCsD5P";
 
 export const globalInstance = axios.create({
-  withCredentials: true,
   baseURL: BASE_URL,
   headers: {
     app_api_key: APP_API_KEY,

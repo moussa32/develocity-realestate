a72f19d118dd6c3c5e94c0c67a93e521a2aad6cc
mozes32 <moussaebrahem32@gmail.com>
2022-10-09 14:01:42 +0200

    refactor: change categories endpoint

--- src/redux/features/CategorySlice.js
+++ src/redux/features/CategorySlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { globalInstance } from "../../pages/DataFetch/constat";
 
 export const fetchCategory = createAsyncThunk("category/fetchCategoryData", async (categoryID) => {
-  const categoryParams = parseInt(categoryID) ? `category/${categoryID}/advertisements` : `category/advertisements`;
+  const categoryParams = parseInt(categoryID) ? `category/${categoryID}/realstates` : `category/realstates`;
   const request = await globalInstance.get(categoryParams);
 
   return request.data;

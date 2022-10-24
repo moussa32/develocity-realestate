2e53c35be257effb31b97cbf1fcd244f7b332b59
mozes32 <moussaebrahem32@gmail.com>
2022-10-24 20:04:40 +0200

    feat: added facebook login

--- src/modules/Auth/ThirdPartyButtons.js
+++ src/modules/Auth/ThirdPartyButtons.js
@@ -4,6 +4,7 @@ import { BsTelephoneFill } from "react-icons/bs";
 import styles from "./ThirdPartyButtons.module.scss";
 import { useDispatch } from "react-redux";
 import { setShowModal } from "../../redux/features/ModalSlice";
+import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
 
 const ThirdPartyButtons = () => {
   const dispatch = useDispatch();
@@ -12,14 +13,27 @@ const ThirdPartyButtons = () => {
     dispatch(setShowModal("phone"));
   };
 
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center mx-auto gap-4 mt-4">
       <button className={`border border-primary bg-transparent rounded ${styles.thirdPartyButton}`}>
         <FcGoogle className={`${styles.thirdPartyIcon}`} />
       </button>
-      <button className={`border border-primary bg-transparent rounded ${styles.thirdPartyButton}`}>
-        <FaFacebookF className={`${styles.thirdPartyIcon} ${styles.facebookIcon}`} />
-      </button>
+      <FacebookLogin
+        appId="1523259471459515"
+        callback={responseFacebook}
+        render={(renderProps) => (
+          <button
+            className={`border border-primary bg-transparent rounded ${styles.thirdPartyButton}`}
+            onClick={renderProps.onClick}
+          >
+            <FaFacebookF className={`${styles.thirdPartyIcon} ${styles.facebookIcon}`} />
+          </button>
+        )}
+      />
       <button
         className={`border border-primary bg-transparent rounded ${styles.thirdPartyButton}`}
         onClick={handleOpenPhoneModal}
--- src/modules/Profile/Profile.js
+++ src/modules/Profile/Profile.js
@@ -24,10 +24,19 @@ const Profile = () => {
   }, []);
 
   const ProfileLoader = (props) => (
-    <ContentLoader speed={1} viewBox="0 0 400 560" {...props} width={400}>
+    <ContentLoader
+      speed={1}
+      viewBox="0 0 400 560"
+      {...props}
+      width={400}
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+      className="mx-auto d-block"
+      style={{ zIndex: "5000" }}
+    >
       <circle cx="200" cy="200" r="200" style={{ zIndex: "5000" }} />
-      <rect x="100" y="440" rx="0" ry="0" width="200" height="25" />
-      <rect x="80" y="495" rx="0" ry="0" width="240" height="15"></rect>
+      <rect x="100" y="440" rx="0" ry="0" width="200" height="25" style={{ zIndex: "5000" }} />
+      <rect x="80" y="495" rx="0" ry="0" width="240" height="17" style={{ zIndex: "5000" }} />
     </ContentLoader>
   );
 
